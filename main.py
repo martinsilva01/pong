55975a5f45c2430217dbcc2638039bd26e9f396e
Martin Silva <Martin.Silva01@student.csulb.edu>
2024-09-25 11:30:28 -0700

    started main, added screen, added first paddle

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
+import sys
 import pygame
 
+
+class Display:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.surface = pygame.display.set_mode((width, height))
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+
+class Paddle():
+    def __init__(self, start_x, start_y):
+        self.color = (255, 255, 255)
+        self.rect = pygame.Rect((start_x, start_y), (25, 250))
+
+
+
 def main():
-    
+    pygame.init()
+    d = Display(1920, 1080)
+    clock = pygame.time.Clock()
+    running = True
+
+    l_paddle = Paddle(100, 500)
+
+
+    while running:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.quit:
+                running = False
+                sys.exit()
+
+        d.surface.fill((0, 0, 0))
+        pygame.draw.rect(d.surface, l_paddle.color, l_paddle.rect)
+        pygame.display.flip()
+
+
 if __name__ == '__main__':
     main()
