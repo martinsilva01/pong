cd8462d0d3c5a41b6f2d3333c4ddcd9adf763460
drnpf <captaindj03@gmail.com>
2024-11-18 20:59:44 -0800

    audio files and setup

--- audio.py
+++ audio.py
@@ -2,21 +2,24 @@ import pygame
 
 pygame.mixer.init()
 
-UI_BUTTON_SOUND_PATH = ""
-PADDLE_HIT_SOUND_PATH = ""
-WALL_HIT_SOUND_PATH = ""
-SCORE_SOUND_PATH = ""
-BACKGROUND_MUSIC_PATH = ""
+UI_BUTTON_SOUND_PATH = "assets/selection.wav"
+PADDLE_HIT_SOUND_PATH = "assets/collision.wav"
+WALL_HIT_SOUND_PATH = "assets/wallcollision.wav"
+SCORE_SOUND_PATH = "assets/score.wav"
+POWER_UP_SOUND_PATH = "assets/powerUp.wav"
+BACKGROUND_MUSIC_PATH = "assets/ponggameBGM.wav"
 
 button_select_sound = pygame.mixer.Sound(UI_BUTTON_SOUND_PATH)
 paddle_hit_sound = pygame.mixer.Sound(PADDLE_HIT_SOUND_PATH)
 wall_hit_sound = pygame.mixer.Sound(WALL_HIT_SOUND_PATH)
 score_sound = pygame.mixer.Sound(SCORE_SOUND_PATH)
+power_up_sound = pygame.mixer.Sound(POWER_UP_SOUND_PATH)
 
-button_select_sound.set_volume(0.5)
-paddle_hit_sound.set_volume(0.5)
-wall_hit_sound.set_volume(0.5)
-score_sound.set_volume(0.5)
+button_select_sound.set_volume(0.15)
+paddle_hit_sound.set_volume(0.2)
+wall_hit_sound.set_volume(0.2)
+score_sound.set_volume(0.2)
+power_up_sound.set_volume(0.2)
 
 def play_sound(sound):
     sound.play()
